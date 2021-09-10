77ec835cf3f6eeabf320149e8da54a6a34addaa0
Jestin Rodriguez <jestinrenzo.rodriguez@gmail.com>
2021-09-09 22:05:37 -0700

    added a dropdown to sort by icon

--- src/pages/Dashboard/components/DashboardSort.js
+++ src/pages/Dashboard/components/DashboardSort.js
@@ -2,19 +2,44 @@
    Dashboard Sort Component 
  */
 
-import React from 'react';
-import { Note2Grey } from 'common';
-import { iconSort } from 'media/svg';
+import React from "react";
+import { Note2Grey } from "common";
+import { iconSort } from "media/svg";
+
+import { Menu, Dropdown, message } from "antd";
 
 const DashboardSort = () => {
+  const onClick = ({ key }) => {
+    message.info(`Sort items by ${key}`);
+  };
+
+  const sortMenu = (
+    <Menu onClick={onClick}>
+      <Menu.Item key="Renewal Date">
+        <Note2Grey>Renewal Date</Note2Grey>
+      </Menu.Item>
+      <Menu.Item key="Account Created">
+        <Note2Grey>Account Created</Note2Grey>
+      </Menu.Item>
+      <Menu.Item key="Highest score">
+        <Note2Grey>Highest score</Note2Grey>
+      </Menu.Item>
+      <Menu.Item key="Lowest score">
+        <Note2Grey>Lowest score</Note2Grey>
+      </Menu.Item>
+      <Menu.Item key="Contract size">
+        <Note2Grey>Contract size</Note2Grey>
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
-    <div>
-      <div>
-        <img src={iconSort}
-          alt="" />
-        <Note2Grey>Sort By</Note2Grey>
+    <Dropdown overlay={sortMenu}>
+      <div className="sortBy">
+        <img src={iconSort} alt="" />
+        <Note2Grey> Sort By </Note2Grey>
       </div>
-    </div>
+    </Dropdown>
   );
 };
 
